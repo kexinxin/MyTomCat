468af29633ebc582b0318b3f83edb3bb1499a67c
kexinxin <1051202524@qq.com>
2019-11-05 15:35:31 +0800

    commit

---
+++ src/myTomcat/startup/Bootstrap.java
@@ -0,0 +1,10 @@
+package myTomcat.startup;
+
+import myTomcat.connector.http.HttpConnector;
+
+public final class Bootstrap {
+  public static void main(String[] args) {
+    HttpConnector connector = new HttpConnector();
+    connector.start();
+  }
+}
